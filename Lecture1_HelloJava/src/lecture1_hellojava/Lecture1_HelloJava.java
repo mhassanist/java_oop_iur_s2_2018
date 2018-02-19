ade26e041713028e9e86e882b5eab9df5259cf2b
mhassanist <m.hassan@ogr.iu.edu.tr>
2018-02-19 16:35:02 +0300

    First Lecture

---
+++ Lecture1_HelloJava/src/lecture1_hellojava/Lecture1_HelloJava.java
@@ -0,0 +1,31 @@
+package lecture1_hellojava;
+/**
+ * @author MSAUDI
+ */
+public class Lecture1_HelloJava {
+   
+    /**
+     * This method is the main method of the application.
+     * Application starts here
+     * @param args The command line argument if any
+     */
+    public static void main(String[] args) {
+        //Your code starts here. This is a one line comment
+        /* 
+        This is 
+        a 
+        multiline comment
+        */
+        System.out.println("\"Hello Mohammed\",He Said");
+
+        System.out.println("******");
+        System.out.println("* OV *");
+        System.out.println("******");
+        System.out.println("  **  ");
+        System.out.println("  **  ");
+        System.out.println("  **  ");
+
+        System.out.println("******\n* OV *\n******\n  **  \n  **  \n  **  ");
+    }
+    
+}
---
+++ Lecture1_HelloJava/src/lecture1_hellojava/Lecture_Variables_1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture1_hellojava;
+
+/**
+ *
+ * @author MSAUDI
+ */
+public class Lecture_Variables_1 {
+     public static void main(String[] args) {
+             
+         boolean b;
+         byte by;
+         char c;
+         short s;
+         int i;
+         long l;
+         float f;
+         double d;
+         
+         b= true;
+         b= false;
+         
+         by = 127;
+        
+         c = 'b';
+         
+     }
+}
